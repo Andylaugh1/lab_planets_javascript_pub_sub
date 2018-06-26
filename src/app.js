7f8fef647191327d3c18a3aa14459bcc194c1f07
Andy Laughlin <atl11384@gmail.com>
2018-06-26 15:40:49 +0100

    result view created but not quite finished

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const planetsData = require('./data/planets.js');
 const SolarSystem = require('./models/solar_system.js');
-const InputView = require('./views/input_view.js')
+const InputView = require('./views/input_view.js');
+const ResultView = require('./views/result_view.js');
 
 document.addEventListener('DOMContentLoaded', () => {
   const planetsDataModel = new SolarSystem(planetsData);
   const inputView = new InputView();
+  const resultView = new ResultView();
 
   inputView.getPlanetSelection()
   planetsDataModel.publishPlanetDetails()
+  resultView.getPlanetInfo()
 
 
 
--- src/views/result_view.js
+++ src/views/result_view.js
@@ -1,5 +1,23 @@
+const PubSub = require('../helpers/pub_sub.js');
+
 const ResultView = function () {
 
 }
+
+ResultView.prototype.getPlanetInfo = function () {
+  PubSub.subscribe('PlanetInfo:result', (event) => {
+    const planetObject = event.detail;
+    console.log(planetObject);
+    this.displayResult(planetObject);
+  })
+
+  ResultView.prototype.displayResult = function (result) {
+    const planetInfo = document.createElement('.planet-details');
+    planetInfo.textContent = 
+    this.container.appendChild(planetInfo);
+
+  };
+
+};
 
 module.exports = ResultView;
